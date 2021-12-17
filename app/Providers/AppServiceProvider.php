2e558e74b6d74c01793c44a69e3465816103f6f8
vagrant <vagrant@homestead>
2021-12-17 01:00:07 +0000

    heroku fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,4 +26,12 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+
+
+public function boot()
+{
+　　//以下を記述
+    Schema::defaultStringLength(191);
+}
 }
